d41b52695a427117f4090e762743d7c87b768c06
Flyone <1508948470@qq.com>
2023-03-13 18:31:37 +0800

    解决跨域

--- api/proxy.js
+++ api/proxy.js
@@ -8,7 +8,7 @@ module.exports = (req, res) => {
     // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
     // target 替换为你跨域请求的服务器 如： http://gmall-h5-api.atguigu.cn
     if (req.url.startsWith('/api')) {
-        target = 'http://124.221.121.35:8080/'
+        target = 'http://124.221.121.35:8080/api'
     }
     // 创建代理对象并转发请求
     createProxyMiddleware({
